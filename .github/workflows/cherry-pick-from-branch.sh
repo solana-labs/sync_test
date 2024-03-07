e7a2490f8713d4bc501c95c1f73c6be93fb0a20e
GHA: Update Upstream From Fork <will.hickey@anza.xyz>
2024-03-07 16:47:37 -0600

    Cleanup

--- .github/workflows/cherry-pick-from-branch.sh
+++ .github/workflows/cherry-pick-from-branch.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-# Shell script:
-# input: sha_before, last_sha
-# Checkout temp branch
-# for sha in sha_before..last_sha
-#   If commit message doesn't contain FOOBAR:
-#     Cherry pick sha to temp branch
-# Push upstream master
-
-
-# TODO a way to skip a commit (CI migration, etc)
-# TODO all referencs to "master" need to be dynamic
-
+# Why
+# What
+# Assumptions:
+# In a repo
+# $DEST_REPO/$DEST_BRANCH is defined, readable, and writeable
+# $BEFORE_SHA..$LAST_SHA must define a sequence of commits
 
 DEST_REMOTE=$1
 DEST_BRANCH=$2
@@ -24,15 +18,15 @@ echo "LAST_SHA: $LAST_SHA"
 
 SKIP_COMMIT_STRING="DO NOT SYNC"
 
-git config --global user.email "will.hickey@anza.xyz"
-git config --global user.name "GHA: Update Upstream From Fork"
+git config --global user.email "noreply@anza.xyz"
+git config --global user.name "GHA: Update Upstream From Fork" #TODO better name
 git fetch --all
 echo "-------------------------"
-echo "git log --oneline remotes/upstream/master:"
-git log --oneline remotes/upstream/master
+echo "git log --oneline remotes/$DEST_REMOTE/$DEST_BRANCH:"
+git log --oneline "remotes/$DEST_REMOTE/$DEST_BRANCH"
 echo "-----------------------"
-echo "git log --oneline origin/master"
-git log --oneline origin/master
+echo "git log --oneline $BEFORE_SHA~..$LAST_SHA"
+git log --oneline "$BEFORE_SHA~..$LAST_SHA"
 echo "-------------------------"
 git branch -D temp_branch
 git checkout -b temp_branch "remotes/$DEST_REMOTE/$DEST_BRANCH"
@@ -49,5 +43,4 @@ for sha1 in $(git log --reverse --format=format:%H $BEFORE_SHA..$LAST_SHA); do
         echo "Commit message contains $SKIP_COMMIT_STRING. Skipping..."
     fi
 done
-# git cherry-pick remotes/upstream/master..origin/master
-# git push upstream master
+git push "$DEST_REMOTE" "$DEST_BRANCH"
